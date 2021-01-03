e5ec15e1b302a2015c6946e07be471d31f252b82
TounsiChaima <61359093+TounsiChaima@users.noreply.github.com>
2021-01-03 20:35:27 +0100

    Add files via upload

---
+++ src/data/meals.js
@@ -0,0 +1,72 @@
+const meals = [
+    {
+      id: "5b21ca3eeb7f6fbccd471ddskls",
+      name :'Tajine',
+      dsc : "Tajine Tunsien : bonne source de proteine et de glucides pour votre dîner.",
+      image:'../images/tajine.jpg',
+      type : "item dinner"
+      
+    },
+    {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Salde aux fruits de mer',
+        dsc : "Savourez le vrai goût des fruits de mer avec une salade équilibrée.",
+        image:'../images/food2.jpg',
+        type : "item dinner special"
+        
+      },
+      {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Banana Bread',
+        dsc : "Commencez votre journée avec un petit déj délicieux qui va booster votre energie !",
+        image:'../images/banana-bread.jpg',
+        type : "item special breakfast"
+        
+      },
+      {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Lablabi',
+        dsc : "Lablabi Healthy, tout est possible avec nos recettes étudiées!",
+        image:'../images/lablabi.jpg',
+        type : "item dinner"
+        
+      },
+      {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Poissons Grillés',
+        dsc : " Un dîner gourmand et nutritif avec un plat de Poissons Grillés",
+        image:'../images/poissons.jpg',
+        type : "item dinner"
+        
+      },
+      {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Pâtes aux legumes',
+        dsc : "Ajoutons des legumes à vos pâte pour avoir un bon équilibre !",
+        image:'../images/food7.jpg',
+        type : "item dinner"
+        
+      },
+      {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Salade Speciale',
+        dsc : "Ingrédients simples et nutritifs que vous n'avez jamais pensé à combiner!",
+        image:'../images/food8.jpg',
+        type : "item dinner special"
+        
+      },
+      {
+        id: "5b21ca3eeb7f6fbccd471ddskls",
+        name :'Crêpes',
+        dsc : "Un dessert gourmand et healthy à la fois !",
+        image:'../images/crepes.jpg',
+        type : "item breakfast desert"
+        
+      }
+]
+
+
+export default meals;
+
+
+
